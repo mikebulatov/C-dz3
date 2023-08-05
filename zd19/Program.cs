50f63346c9caac312d0072515d7ec4f1bca58c4b
Mikebulatov <mikebulatov@yandex.ru>
2023-08-05 11:51:42 +0700

    Задача 19. Изменение 1

--- zd19/Program.cs
+++ zd19/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача 19. Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
+
+Console.Write("Введите  число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num<10000 || num>99999 )
+
+{
+    Console.WriteLine("Число не пятизначное");
+}
+else
+{
+string str = Convert.ToString(num);
+    if (str[0]==str[4]&& str[1]==str[3])
+    {
+        Console.WriteLine("Число "+str+" палиндром");
+    }
+    else
+    {
+         Console.WriteLine("Число "+str+" не палиндром");
+    }
+
+}
