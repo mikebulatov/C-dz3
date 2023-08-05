61041d59cd08023a42ac952435d6e5fd8f2d3375
Mikebulatov <mikebulatov@yandex.ru>
2023-08-05 12:00:54 +0700

    Задача 23

---
+++ zd23/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задача 23. Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+
+Console.Write("Введите  число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num<10000 || num>99999 )
+
+{
+    Console.WriteLine("Число не пятизначное");
+}
+else
+{
+string str = Convert.ToString(num);
+    if (str[0]==str[4]&& str[1]==str[3])
+    {
+        Console.WriteLine("Число "+str+" палиндром");
+    }
+    else
+    {
+         Console.WriteLine("Число "+str+" не палиндром");
+    }
+
+}
