3e37c534b3a4c2a6a98ec30391e8b43721a4d806
Mikebulatov <mikebulatov@yandex.ru>
2023-08-05 12:39:17 +0700

    Задача 21. Изменение 1.

--- zd21/Program.cs
+++ zd21/Program.cs
@@ -1,24 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Задача 21. Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.");
 
-Console.Write("Введите  число:");
-int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  кординаты 1й точки:");
+Console.Write(" x1=");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" y1=");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" z1=");
+int z1 = Convert.ToInt32(Console.ReadLine());
 
-if(num<10000 || num>99999 )
+Console.WriteLine("Введите  кординаты 2й точки:");
+Console.Write(" x2=");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" y2=");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write(" z2=");
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-{
-    Console.WriteLine("Число не пятизначное");
-}
-else
-{
-string str = Convert.ToString(num);
-    if (str[0]==str[4]&& str[1]==str[3])
-    {
-        Console.WriteLine("Число "+str+" палиндром");
-    }
-    else
-    {
-         Console.WriteLine("Число "+str+" не палиндром");
-    }
 
-}
+Console.WriteLine("Расстояние между ними в 3D пространстве:"+Convert.ToString(Math.Pow((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) * 1.0), 0.5)));
