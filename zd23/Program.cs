1dd886bc10397cff78836ab4bd8f99be2fe53891
Mikebulatov <mikebulatov@yandex.ru>
2023-08-05 12:14:26 +0700

    Задача 23. Изменение 1.

--- zd23/Program.cs
+++ zd23/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Задача 23. Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
 
 int i,N;
-Console.Write("Введите  число:");
+Console.Write("Введите  число N:");
 N = Convert.ToInt32(Console.ReadLine());
 Console.Write("Таблица кубов чисел от 1 до "+Convert.ToString(N)+":::: ");
 for(i=1;i<=N;i++)
